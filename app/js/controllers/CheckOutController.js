f5a5626988bb94773982c0e672e7567da43f1448
Caraway <m-begale@northwestern.edu>
2016-08-11 11:35:19 -0500

    finish prototype for part 3 part 2

--- app/js/controllers/CheckOutController.js
+++ app/js/controllers/CheckOutController.js
@@ -5,58 +5,67 @@
 
     this.confirmationVisible = false;
 
+    this.child_name = 'Maia';
+
     this.showConfirmation = function(){
       this.confirmationVisible = true;
     }
 
     this.activity_setting = null;
 
+    this.participation_change_types = {
+      do_more_often: null,
+      do_less_often: null,
+      be_more_helpful: null,
+      be_more_interactive: null,
+      do_a_broader_variety: null
+    }
+
     this.pem = {};
 
     this.pem.activities = [
-
-      {
-        group:'Home', 
-        activities:[                
-          'Getting rest',
-          'Personal care management',
-          'Getting clean', 
-          'Mealtime',
-          'Cleaning up',
-          'Meal preparation',
-          'Taking care of other family members',
-          'Dishes/ laundry',
-          'Arts, crafts, stories, music',
-          'Screen time',
-          'Indoor play / games',
-          'Celebrations at home',
-          'House guests'
-        ]
-      },      
-      {
-        group:'Daycare/Preschool', 
-        activities:[
-          'Group learning',
-          'Field trips/ events',
-          'Socializing with friends'
-        ]
-      },{
-        group:'Community', 
-        activities:[ 
-          'Shopping/ errands',
-          'Dining out',
-          'Routine appointments',
-          'Classes/ lessons',
-          'Organized physical activities',
-          'Community attractions',
-          'Religious/ spiritual gatherings',
-          'Social gatherings',
-          'Community events',
-          'Unstructured physical activities',
-          'Overnight visits/ trips'
-        ]
-      },
-    ];
+    {
+      group:'Home', 
+      activities:[                
+        'Getting rest',
+        'Personal care management',
+        'Getting clean', 
+        'Mealtime',
+        'Cleaning up',
+        'Meal preparation',
+        'Taking care of other family members',
+        'Dishes/ laundry',
+        'Arts, crafts, stories, music',
+        'Screen time',
+        'Indoor play / games',
+        'Celebrations at home',
+        'House guests'
+      ]
+    },      
+    {
+      group:'Daycare/Preschool', 
+      activities:[
+        'Group learning',
+        'Field trips/ events',
+        'Socializing with friends'
+      ]
+    },{
+      group:'Community', 
+      activities:[ 
+        'Shopping/ errands',
+        'Dining out',
+        'Routine appointments',
+        'Classes/ lessons',
+        'Organized physical activities',
+        'Community attractions',
+        'Religious/ spiritual gatherings',
+        'Social gatherings',
+        'Community events',
+        'Unstructured physical activities',
+        'Overnight visits/ trips'
+      ]
+    }
+  ];
 
   this.activityPriorityMoveDown = function(index) {
     //would move the element in the array down one position
@@ -70,8 +79,14 @@
 
     var activities = $filter('filter')(this.pem.activities,{group:this.activity_setting})[0]
     return activities
-
   }
+
+  this.data = {};
+  this.data.part1 = {};
+  this.data.part2 = {
+      selected_activity: this.pem.activities[1].activities[1]
+  };
+
 }
 
   angular.module('cbitsPrototype.controllers')
